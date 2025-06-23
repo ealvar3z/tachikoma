5422e6c502c0ca094adf106e17f9e69335b942ca
ealvar3z <55966724+ealvar3z@users.noreply.github.com>
2025-06-23 00:22:35 -0400

    fmt home.nix

--- home.nix
+++ home.nix
@@ -236,177 +236,177 @@
       '';
     };
 
-      programs = {
-        bash = {
-          enable = true;
-          historyFile = "$HOME/.history";
-          historyControl = [
-            "erasedups"
-            "ignoredups"
-            "ignorespace"
-          ];
-          bashrcExtra = ''
-            export PS1="; "
-          '';
-        };
-        neovim = {
-          enable = true;
-          viAlias = true;
-          vimAlias = true;
-          coc = {
-            enable = true;
-            settings = {
-              languageserver = {
-                nix = {
-                  command = "nil";
-                  filetypes = [ "nix" ];
-                  rootPatterns = [ "flake.nix" ];
-                  settings.nil.formatting.command = [ "nixfmt" ];
-                };
-                go = {
-                  command = "gopls";
-                  filetypes = [ "go" ];
-                  rootPatterns = [
-                    "go.work"
-                    "go.mod"
-                    ".vim/"
-                    ".git/"
-                    ".hg/"
-                  ];
-                };
-              };
-              coc.preferences.formatOnSaveFiletypes = [
-                "nix"
-                "go"
-              ];
-            };
-          };
-
-          extraConfig = ''
-            set termguicolors
-            set clipboard=unnamedplus
-            set syntax=off
-          '';
-
-          plugins = with pkgs.vimPlugins; [
-            {
-              plugin = vacme-vim;
-              config = "colorscheme vacme";
-            }
-            {
-              plugin = nvim-lastplace;
-              config = ''
-                lua require'nvim-lastplace'.setup{}
-                let g:lastplace_ignore_buftype = "quickfix,nofile,help"
-                let g:lastplace_ignore_filetype = "gitcommit,gitrebase,svn,hgcommit"
-                let g:lastplace_open_folds = 1
-              '';
-            }
-            vim-go
-          ];
-        };
-        foot = {
+    programs = {
+      bash = {
+        enable = true;
+        historyFile = "$HOME/.history";
+        historyControl = [
+          "erasedups"
+          "ignoredups"
+          "ignorespace"
+        ];
+        bashrcExtra = ''
+          export PS1="; "
+        '';
+      };
+      neovim = {
+        enable = true;
+        viAlias = true;
+        vimAlias = true;
+        coc = {
           enable = true;
           settings = {
-            main = {
-              term = "xterm-256color";
-              font = "VGA:size=12";
-              font-bold = "VGA:size=12";
-              font-italic = "VGA:size=12";
-            };
-            cursor.color = "45363b 45363b";
-            colors = {
-              background = "FFFFFF";
-              foreground = "45363b";
-
-              regular0 = "20111a";
-              regular1 = "bd100d";
-              regular2 = "858062";
-              regular3 = "e9a448";
-              regular4 = "416978";
-              regular5 = "96522b";
-              regular6 = "98999c";
-              regular7 = "958b83";
-
-              bright0 = "5e5252";
-              bright1 = "bd100d";
-              bright2 = "858062";
-              bright3 = "e9a448";
-              bright4 = "416978";
-              bright5 = "96522b";
-              bright6 = "98999c";
-              bright7 = "d4ccb9";
-            };
-          };
-        };
-      };
-
-      services = {
-        mako = {
-          enable = true;
-          font = "VGA 12";
-          anchor = "top-left";
-          output = "HDMI-A-1";
-          backgroundColor = "#FFFFFFFF";
-          textColor = "#000000FF";
-          borderColor = "#55aaaaFF";
-          borderSize = 4;
-          format = "%s\\n%b";
-        };
-      };
-
-      wayland.windowManager.sway = {
-        enable = true;
-        #unfortunately we can not use these flags with homemanger
-        config = {
-          modifier = "Mod4";
-          terminal = "foot";
-          bars = [ ];
-          colors = {
-            focused = {
-              background = "#000000";
-              border = "#55aaaa";
-              text = "#000000";
-              childBorder = "#55aaaa";
-              indicator = "#000000";
-            };
-            unfocused = {
-              background = "#b2b2b2";
-              border = "#9eeeee";
-              text = "#000000";
-              childBorder = "#9eeeee";
-              indicator = "#000000";
-            };
-          };
-          window.commands = [
-            {
-              command = "border pixel 4";
-              criteria = {
-                title = "[.]*";
-                app_id = "[.]*";
-                class = "[.]*";
+            languageserver = {
+              nix = {
+                command = "nil";
+                filetypes = [ "nix" ];
+                rootPatterns = [ "flake.nix" ];
+                settings.nil.formatting.command = [ "nixfmt" ];
               };
-            }
-          ];
-          # tiling is for nerds
-          floating.criteria = [
-            { title = "[.]*"; }
-            { app_id = "[.]*"; }
-            { class = "[.]*"; }
-          ];
-          output =
-            {
-              "tachikoma" = {
-                HDMI-A-1 = {
-                  bg = "#b2b2b2 solid_color";
-                  mode = "1920x1080@60Hz";
-                  pos = "5120 700";
-                  transform = "normal";
-                };
-            }
-            ."${config.networking.hostName}";
+              go = {
+                command = "gopls";
+                filetypes = [ "go" ];
+                rootPatterns = [
+                  "go.work"
+                  "go.mod"
+                  ".vim/"
+                  ".git/"
+                  ".hg/"
+                ];
+              };
+            };
+            coc.preferences.formatOnSaveFiletypes = [
+              "nix"
+              "go"
+            ];
+          };
+        };
+
+        extraConfig = ''
+          set termguicolors
+          set clipboard=unnamedplus
+          set syntax=off
+        '';
+
+        plugins = with pkgs.vimPlugins; [
+          {
+            plugin = vacme-vim;
+            config = "colorscheme vacme";
+          }
+          {
+            plugin = nvim-lastplace;
+            config = ''
+              lua require'nvim-lastplace'.setup{}
+              let g:lastplace_ignore_buftype = "quickfix,nofile,help"
+              let g:lastplace_ignore_filetype = "gitcommit,gitrebase,svn,hgcommit"
+              let g:lastplace_open_folds = 1
+            '';
+          }
+          vim-go
+        ];
+      };
+      foot = {
+        enable = true;
+        settings = {
+          main = {
+            term = "xterm-256color";
+            font = "VGA:size=12";
+            font-bold = "VGA:size=12";
+            font-italic = "VGA:size=12";
+          };
+          cursor.color = "45363b 45363b";
+          colors = {
+            background = "FFFFFF";
+            foreground = "45363b";
+
+            regular0 = "20111a";
+            regular1 = "bd100d";
+            regular2 = "858062";
+            regular3 = "e9a448";
+            regular4 = "416978";
+            regular5 = "96522b";
+            regular6 = "98999c";
+            regular7 = "958b83";
+
+            bright0 = "5e5252";
+            bright1 = "bd100d";
+            bright2 = "858062";
+            bright3 = "e9a448";
+            bright4 = "416978";
+            bright5 = "96522b";
+            bright6 = "98999c";
+            bright7 = "d4ccb9";
+          };
         };
       };
     };
+
+    services = {
+      mako = {
+        enable = true;
+        font = "VGA 12";
+        anchor = "top-left";
+        output = "HDMI-A-1";
+        backgroundColor = "#FFFFFFFF";
+        textColor = "#000000FF";
+        borderColor = "#55aaaaFF";
+        borderSize = 4;
+        format = "%s\\n%b";
+      };
+    };
+
+    wayland.windowManager.sway = {
+      enable = true;
+      #unfortunately we can not use these flags with homemanger
+      config = {
+        modifier = "Mod4";
+        terminal = "foot";
+        bars = [ ];
+        colors = {
+          focused = {
+            background = "#000000";
+            border = "#55aaaa";
+            text = "#000000";
+            childBorder = "#55aaaa";
+            indicator = "#000000";
+          };
+          unfocused = {
+            background = "#b2b2b2";
+            border = "#9eeeee";
+            text = "#000000";
+            childBorder = "#9eeeee";
+            indicator = "#000000";
+          };
+        };
+        window.commands = [
+          {
+            command = "border pixel 4";
+            criteria = {
+              title = "[.]*";
+              app_id = "[.]*";
+              class = "[.]*";
+            };
+          }
+        ];
+        # tiling is for nerds
+        floating.criteria = [
+          { title = "[.]*"; }
+          { app_id = "[.]*"; }
+          { class = "[.]*"; }
+        ];
+        output =
+          {
+            "tachikoma" = {
+              HDMI-A-1 = {
+                bg = "#b2b2b2 solid_color";
+                mode = "1920x1080@60Hz";
+                pos = "5120 700";
+                transform = "normal";
+              };
+          }
+          ."${config.networking.hostName}";
+      };
+    };
+  };
 
 }
